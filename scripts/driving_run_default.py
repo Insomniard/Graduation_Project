ef67482fd2253cf99880bfb11cab02b34445f9dc
Jiyoung <51487025+bonohbonoh@users.noreply.github.com>
2021-10-28 02:02:01 +0900

    Update driving_run_default.py

--- scripts/driving_run_default.py
+++ scripts/driving_run_default.py
@@ -13,24 +13,12 @@ from std_msgs.msg import Int8 , Bool
 
 
 
-#  self.Cset_aimode = 2 # 0:OFF, 1:JustSteer, 2:CarPairToPCReady, 3:ForceControl(Warnning)
-#         self.Cset_drvmode = 0 # 0:Normal, 1:Forward, 2:Reverse, 3:break
-#         self.Cset_throttle = 0 # 0~99 throttle level
-#         self.Cset_steer_degree = 90 # 0~180 steer degree mode set (180 upper is off)
-#         self.Cset_signal = 3 # 0:signal OFF, 1:left signal lamp on, 2:right signal lamp on, 3:both signal lamp on
-#         self.Cset_lamp = 0 # 0:lamp off, 1:lamp on
-#         self.Cset_horn = 0 # 0:horn off, 1:horn on
 
 class my_script_run:
     def __init__(self):
         self.steer_degree_plma = 50
         rospy.init_node('control_programer', anonymous=True)
-        self.carDrv         = cardrv_control_example("gilbot_control_cli") #driver code confusion.. import for simple coding.
-       # self.cam_sub_0      = rospy.Subscriber("/camera0/compressed", CompressedImage, self.callback_cam0) #read for cam0 image.
-        # self.gps_sub_utm    = rospy.Subscriber("/gps_node/get_UTM", NavSatFix, self.callback_utm)
-        # self.gps_sub_time   = rospy.Subscriber("/gps_node/get_time", String, self.callback_time)
-        # self.gps_sub_speed  = rospy.Subscriber("/gps_node/get_speed", String, self.callback_speed)
-        # self.lidar_sub      = rospy.Subscriber("/scan", LaserScan, self.callback_laser)
+        self.carDrv         = cardrv_control_example("gilbot_control_cli") 
         self.img_bgr_0 = None
         rospy.Subscriber('ridar_ctr_left',Bool,self.callback_lidar_left)
         rospy.Subscriber('ridar_ctr_right',Bool,self.callback_lidar_right)
@@ -46,9 +34,7 @@ class my_script_run:
         
         self.rate = rospy.Rate(20) #car drv control rate hz (0.05s)
         self.Cset_aimode = 2
-        ## 1 : 왼쪽헨들 2: 우짝헨들 3(A):빵빵   4(B): 우짝깜빡이 5(X): 좌짝깜빡이 6(Y): 램프 7(LT): 속도2배
         while not rospy.is_shutdown():
-            # print(self.left_point.data,self.right_point.data,self.stop_point.data)
             if self.left_point.data == False and self.right_point.data == False and self.stop_point.data == False:
                 self.is_cb = True
             else : self.is_cb = False
@@ -93,49 +79,7 @@ class my_script_run:
             if self.is_cb == False and self.stop_point.data == True:
                     self.carDrv.Cset_throttle = 0
                     self.carDrv.Cset_drvmode=3
-
-                
-            #print("handle h : ",self.hander_point.data)
-                                # if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>1000 and abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT/180))<5000:
-                #     self.carDrv.Cset_steer_degree = 105
-                #     self.Cset_steer_raw = 9999
-                # if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>5000 and abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT/180))<10000:
-                #     self.carDrv.Cset_steer_degree = 120
-                #     self.Cset_steer_raw = 9999
-                # if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>10000 and abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT/180))<15000:
-                #     self.carDrv.Cset_steer_degree = 135
-                #     self.Cset_steer_raw = 9999
-                # if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>15000 and abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT/180))<20000:
-                #     self.carDrv.Cset_steer_degree = 150
-                #     self.Cset_steer_raw = 9999
-                # if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>20000 and abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT/180))<25000:
-                #     self.carDrv.Cset_steer_degree = 175
-                #     self.Cset_steer_raw = 9999
-                # if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>25000:
-                #     self.carDrv.Cset_steer_degree = 180
-                #     self.Cset_steer_raw = 9999
-
-
-            # if self.joystick.getKey_RIGHT_STICK_LEFTRIGHT>0:
-            #     if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>1000 and abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT/180))<5000:
-            #         self.carDrv.Cset_steer_degree = 75
-            #         self.Cset_steer_raw = 9999
-            #     if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>5000 and abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT/180))<10000:
-            #         self.carDrv.Cset_steer_degree = 60
-            #         self.Cset_steer_raw = 9999
-            #     if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>10000 and abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT/180))<15000:
-            #         self.carDrv.Cset_steer_degree = 45
-            #         self.Cset_steer_raw = 9999
-            #     if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>15000 and abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT/180))<20000:
-            #         self.carDrv.Cset_steer_degree = 30
-            #         self.Cset_steer_raw = 9999
-            #     if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>20000 and abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT/180))<25000:
-            #         self.carDrv.Cset_steer_degree = 15
-            #         self.Cset_steer_raw = 9999
-            #     if abs(int(self.joystick.getKey_RIGHT_STICK_LEFTRIGHT))>25000:
-            #         self.carDrv.Cset_steer_degree = 0
-            #         self.Cset_steer_raw = 9999
-            
+                    
             self.carDrv.Cset_active()
             self.rate.sleep()
     
