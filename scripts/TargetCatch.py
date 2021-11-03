c40f173561999952152298f2690617e0fab4e7db
Jiyoung <51487025+bonohbonoh@users.noreply.github.com>
2021-11-03 17:06:44 +0900

    Update TargetCatch.py

--- scripts/TargetCatch.py
+++ scripts/TargetCatch.py
@@ -5,7 +5,7 @@ import numpy as np
 import time, threading
 from std_msgs.msg import Int8
 class targetcatch:
-    def __init__(self):
+       def __init__(self):
         rospy.init_node('targetcatcher', anonymous=True)
 
         self.pub = rospy.Publisher('/handler_point',Int8, queue_size=1)
@@ -24,8 +24,8 @@ class targetcatch:
         self.cap = cv.VideoCapture(0)
         self.check_point = False
         self.horn_point = False
-        startThread = threading.Thread(target=self.Controlltarget)
-        startThread.start()
+        catch_start = threading.Thread(target=self.Controlltarget)
+        catch_start.start()
 
 
     def get_hander_point(self):
@@ -35,7 +35,6 @@ class targetcatch:
         
         ret, img_color=self.cap.read()
         self.hsv, self.lower_blue1, self.upper_blue1, self.lower_blue2, self.upper_blue2, self.lower_blue3, self.upper_blue3
-        # 마우스 왼쪽 버튼 누를시 위치에 있는 픽셀값을 읽어와서 HSV로 변환합니다.
     
         if event == cv.EVENT_LBUTTONDOWN:
             print(img_color[y, x])
@@ -53,8 +52,6 @@ class targetcatch:
                 self.upper_blue2 = np.array([self.hsv[0], 255, 255])
                 self.lower_blue3 = np.array([self.hsv[0], self.tracking, self.tracking])
                 self.upper_blue3 = np.array([self.hsv[0] + 10, 255, 255])
-                #     print(i-10+180, 180, 0, i)
-                #     print(i, i+10)
             elif self.hsv[0] > 170:
                 print("case2")
                 self.lower_blue1 = np.array([self.hsv[0], self.tracking, self.tracking])
@@ -63,8 +60,6 @@ class targetcatch:
                 self.upper_blue2 = np.array([self.hsv[0] + 10 - 180, 255, 255])
                 self.lower_blue3 = np.array([self.hsv[0] - 10, self.tracking, self.tracking])
                 self.upper_blue3 = np.array([self.hsv[0], 255, 255])
-                #     print(i, 180, 0, i+10-180)
-                #     print(i-10, i)
             else:
                 print("case3")
                 self.lower_blue1 = np.array([self.hsv[0], self.tracking, self.tracking])
@@ -73,9 +68,6 @@ class targetcatch:
                 self.upper_blue2 = np.array([self.hsv[0], 255, 255])
                 self.lower_blue3 = np.array([self.hsv[0] - 10, self.tracking, self.tracking])
                 self.upper_blue3 = np.array([self.hsv[0], 255, 255])
-                #     print(i, i+10)
-                #     print(i-10, i)
-
             print(self.hsv[0])
             print("@1", self.lower_blue1, "~", self.upper_blue1)
             print("@2", self.lower_blue2, "~", self.upper_blue2)
@@ -86,11 +78,9 @@ class targetcatch:
         
         while True:
             ret, img_color = self.cap.read()
-            # ret, frame = cap.read()
             if ret:
                         
                 cv.rectangle(img_color, (140, 40), (485, 450), (0, 0, 255), 5)
-                # img_color = cv.imread('2.jpg')
                 height, width = img_color.shape[:2]
                 img_color = cv.resize(img_color, (width, height), interpolation=cv.INTER_AREA)
 
@@ -117,47 +107,28 @@ class targetcatch:
                         continue
                     x, y, width, height, area = stats[idx]
                     centerX, centerY = int(centroids[0]), int(centroids[1])
-                    # stats의 맨 마지막값이 면적 (area) -> 이 값 활용하여 특정객체 인식
-                    # temp = list(int(area))
-                    # for i in range(0, 100):
-                    #     temp.append(area)
-                    # areamax = max(temp)
-                    # if areamax:
                     
                     if centerX > 140 and centerX < 485 and centerY > 40 and centerY < 450:
                         check_point = True
                         self.hander_point = 0
                         horn_point = False
-                        print("catch target")
                     elif centerX < 140 and centerX>20:
-                        print("Left Missing")
                         self.hander_point = 1
                     elif centerX > 485 and centerX < 620:
                         self.hander_point = 2
-                        print("Right Missing")
                     else:
                         check_point = False
-                        print("Missing target")
                         horn_point = True
                         self.hander_point = 4
                     
                     self.pub_msg.data = self.hander_point
                     self.pub.publish(self.pub_msg)
-                    # print(area)
-                    # print("=========")
-                    # print(centerX)
-                    # print(centerY)
-                    # print("==========")
-                    # if temp(max)==area:
-                   # print("camera h : ",self.hander_point)
                 # 마스크 이미지로 원본 이미지에서 범위값에 해당되는 영상 부분을 획득합니다.
                 img_result = cv.bitwise_and(img_color, img_color, mask=img_mask)
                 cv.imshow('img_color', img_color)
                 # cv.imshow('img_mask', img_mask)
                 cv.imshow('img_result', img_result)
                 
-
-            # ESC 키누르면 종료
                 if cv.waitKey(1) & 0xFF == 27:
                     
                     break
@@ -165,7 +136,5 @@ class targetcatch:
         cv.destroyAllWindows()
 
     
-    
-
 if __name__=='__main__':
     targetcatch()
