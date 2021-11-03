8fcdc9569965f70f5b405dee3e2fab4bbe9ceb0d
Jiyoung <51487025+bonohbonoh@users.noreply.github.com>
2021-11-03 17:04:07 +0900

    Update driving_run_default.py

--- scripts/driving_run_default.py
+++ scripts/driving_run_default.py
@@ -7,48 +7,46 @@ from sensor_msgs.msg import NavSatFix
 from sensor_msgs.msg import CompressedImage
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import String
-from cardrv_use_example import cardrv_control_example
-from TargetCatch import targetcatch
+from cardrv_use_example_jeong import cardrv_control_example
+from TargetCahch import targetcatch
 from std_msgs.msg import Int8 , Bool
 
-
-
-
 class my_script_run:
+    
     def __init__(self):
         self.steer_degree_plma = 50
-        rospy.init_node('control_programer', anonymous=True)
-        self.carDrv         = cardrv_control_example("gilbot_control_cli") 
+        rospy.init_node('letsgetit', anonymous=True)
+        self.carDrv         = cardrv_control_example("gilbot_client", False)
         self.img_bgr_0 = None
         rospy.Subscriber('ridar_ctr_left',Bool,self.callback_lidar_left)
         rospy.Subscriber('ridar_ctr_right',Bool,self.callback_lidar_right)
         rospy.Subscriber('ridar_data_check',Bool,self.callback_lidar_stop)
 
         rospy.Subscriber('/handler_point',Int8,self.cb)
+        rospy.Subscriber('/throtle_val',Int8,self.throttle_callback)
+        self.pub_steer = rospy.Publisher('/value/steer_degree', String, queue_size=3)
         self.left_point = Bool()
         self.right_point = Bool()
         self.stop_point = Bool()
         self.hander_point = Int8()
+        self.throttle_value = Int8()
         self.is_cb = False
-
         
-        self.rate = rospy.Rate(20) #car drv control rate hz (0.05s)
+        self.rate = rospy.Rate(20)
         self.Cset_aimode = 2
         while not rospy.is_shutdown():
             if self.left_point.data == False and self.right_point.data == False and self.stop_point.data == False:
                 self.is_cb = True
             else : self.is_cb = False
-            
+
             if self.is_cb:
                 self.carDrv.Cset_drvmode = 1
-                self.carDrv.Cset_throttle = 10
-                print("Tq",self.hander_point.data)
+                self.carDrv.Cset_throttle = self.throttle_value
                 if self.hander_point.data ==1:
                     self.carDrv.Cset_steer_degree = 145
-                    # self.Cset_steer_raw=9999
+                    self.Cset_steer_raw=9999
                 elif self.hander_point.data == 2:
                     self.carDrv.Cset_steer_degree =  1
-                    # self.Cset_steer_raw=9999
                 
                 elif self.hander_point.data ==0:
                     self.carDrv.Cset_steer_degree=65
@@ -56,33 +54,38 @@ class my_script_run:
                     self.is_cb = False
                     self.carDrv.Cset_throttle = 0
                     self.carDrv.Cset_drvmode=3
-                print(self.carDrv.Cset_steer_degree)
+                self.pub_steer.publish(str(self.carDrv.Cset_steer_degree))
+                self.carDrv.Cset_active()
 
             else:
 
                 if self.left_point.data == True:
 
                     self.carDrv.Cset_drvmode = 1
-                    self.carDrv.Cset_throttle = 10
+                    self.carDrv.Cset_throttle = self.throttle_value
                     self.carDrv.Cset_steer_degree =  1
                 elif self.right_point.data == True:
 
                     self.carDrv.Cset_drvmode = 1
-                    self.carDrv.Cset_throttle = 10
+                    self.carDrv.Cset_throttle = self.throttle_value
                     self.carDrv.Cset_steer_degree = 145
                 elif self.left_point.data == True and self.right_point.data == True and self.stop_point.data == True:
                     self.is_cb = False
-                    self.carDrv.Cset_throttle = 0
+                    self.carDrv.Cset_throttle = self.throttle_value
                     self.carDrv.Cset_drvmode=3
                 else : self.carDrv.Cset_steer_degree= 65 # 75degree
             
             if self.is_cb == False and self.stop_point.data == True:
                     self.carDrv.Cset_throttle = 0
                     self.carDrv.Cset_drvmode=3
-                    
+
             self.carDrv.Cset_active()
             self.rate.sleep()
-    
+
+                   
+    def throttle_callback(self, data):
+        self.throttle_value = data.data
+        print(self.throttle_value)
     def cb(self, data):
         self.hander_point.data = data.data
     def callback_lidar_left(self,data):
